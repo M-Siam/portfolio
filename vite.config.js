017dc158693192ec509308d4679a4b54ee6c49cc
Moniruzzaman Siam <131398515+M-Siam@users.noreply.github.com>
2025-04-22 22:13:37 +0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/futuristic-portfolio/', // For GitHub Pages
+  base: '/futuristic-portfolio/', // Ensure this matches your repo name
 });
